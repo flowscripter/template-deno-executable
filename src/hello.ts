42f9012068e5231a16f7c4688af99ae28f44779d
vectronic <hello.vectronic@gmail.com>
2021-03-14 21:55:51 +0000

    feat: add dependency on library template project

--- src/hello.ts
+++ src/hello.ts
@@ -1,8 +1,10 @@
 import { log } from "../deps.ts";
+import { lib } from "../deps.ts";
 
 /**
- * Logs out `Hello`.
+ * Logs out `Hello` and `World`.
  */
 export function hello(): void {
   log.info("Hello");
+  lib.world();
 }
