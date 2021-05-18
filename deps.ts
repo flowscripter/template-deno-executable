e8968ebcf57278155714e9ad492afdd1c21b5491
vectronic <vectronic@users.noreply.github.com>
2021-05-18 00:41:27 +0000

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as log from "https://deno.land/std@0.91.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.97.0/log/mod.ts";
 export * as lib from "https://raw.githubusercontent.com/flowscripter/template-deno-library/v1.0.5/mod.ts";
