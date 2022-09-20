942179451979ab8d59c1e18428087049610a1be6
vectronic <hello.vectronic@gmail.com>
2022-09-20 14:12:36 +0100

    fix: manually bump version to get past broken dependency version

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as log from "https://deno.land/std@0.154.0/log/mod.ts";
 export * as denoLib from "https://raw.githubusercontent.com/flowscripter/template-deno-library/v1.3.0/mod.ts";
-export * as rustLib from "https://raw.githubusercontent.com/flowscripter/template-deno-rust-library/v1.0.11/mod.ts";
+export * as rustLib from "https://raw.githubusercontent.com/flowscripter/template-deno-rust-library/v1.0.13/mod.ts";
